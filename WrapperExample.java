4050389a2bbd70afdc786d4939c53031bbced91f
aniketamte <aniketamte123@gmail.com>
2025-03-30 21:12:31 +0530

    OOP in Java

--- WrapperExample.java
+++ WrapperExample.java
@@ -1,7 +1,25 @@
-public class WrapperExample {
-      public static void main(String[] args) {
-            int a = 10;
 
-            Integer num = 45;
-      }
+public class WrapperExample {
+
+    public static void main(String[] args) {
+        // //Pass by Value
+        // int a = 10;
+        // int b=20;
+        // Integer num = 45;
+
+        //pass by reference value
+        Integer a = 10;
+        Integer b = 20;
+
+        swap(a, b);
+
+        System.out.println(a + " " + b);
+    }
+
+    //Integer is a final class so it takes directly final value when we perform operation it doesnt show the answer it shows on final value
+    static void swap(Integer a, Integer b) {
+        Integer temp = a;
+        a = b;
+        b = temp;
+    }
 }
