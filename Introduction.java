d0d1833b93a338a4c2fbf929bf28499825d8cf44
aniketamte <aniketamte123@gmail.com>
2025-03-30 11:38:57 +0530

    OOP in java

--- Introduction.java
+++ Introduction.java
@@ -4,7 +4,23 @@ public class Introduction {
     /*
      OOP 1 | Introduction & Concepts - Classes, Objects, Constructors, Keywords
      */
-    public static void main(String[] args) {
 
+     /*
+      * Class ==> A class is a name group of properties and function'
+      */
+
+      //create a class
+
+      class Student{
+            int rollNo[] = new int[5];
+            String name[] = new String[5];
+            float marks[] = new float[5];
+      }
+
+    public static void main(String[] args) {
+      Student Aniket[] = new Student[5];
+      Student Aniket = new Student();
+
+      //Aniket is a reference variable of STudent class
     }
 }
