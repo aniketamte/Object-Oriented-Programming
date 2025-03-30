1d2a8375a91304301998440381afa1e8fd726ad6
aniketamte <aniketamte123@gmail.com>
2025-03-30 20:06:55 +0530

    OOP in Java

--- Introduction.java
+++ Introduction.java
@@ -1,35 +1,38 @@
 
-import java.util.Arrays;
-
-
 public class Introduction {
 
     /*
      OOP 1 | Introduction & Concepts - Classes, Objects, Constructors, Keywords
      */
 
-     /*
+ /*
       * Class ==> A class is a name group of properties and function'
-      */
+     */
+    //create a class
+    static class Student {
 
-      //create a class
+        int rollNo;
+        String name;
+        float marks;
+    }
 
-      class Student{
-            int rollNo;
-            String name;
-            float marks;
-      }
-
-      /*
+    /*
       variable inside the object is called instance variable
       Example => syso(student.rollno)
-      */
-
+     */
     public static void main(String[] args) {
-      Student Aniket[] = new Student[5];
-      Student aniket;
-      System.out.println(Arrays.toString(Aniket));
+        int rollNo[] = new int[5];
+        String name[] = new String[5];
+        float marks[] = new float[5];
 
-      //Aniket is a reference variable of STudent class
+        Student students[] = new Student[5];
+        //new = dynamically allocates memory and return a reference variable to it
+        Student aniket = new Student();
+        //System.out.println(Arrays.toString(aniket.rollNo));
+
+        //Aniket is a reference variable of STudent class
+        System.out.println(aniket.rollNo);
+        System.out.println(aniket.name);
+        System.out.println(aniket.marks);
     }
 }
