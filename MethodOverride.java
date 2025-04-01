38628f9af675f5d4d26d147840979e7c1c78ebc8
aniketamte <aniketamte123@gmail.com>
2025-04-01 22:34:18 +0530

    OOP in Java

---
+++ MethodOverride.java
@@ -0,0 +1,27 @@
+/*
+ *   ### Method Overriding in Inheritance
+ *   =====> When a subclass provides its own implementation of a method already defined in the parent class, it is called method overriding.
+ */
+
+class Animal {
+
+    void makeSound() {
+        System.out.println("Animals make different sounds.");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks.");
+    }
+}
+
+public class MethodOverride {
+
+    public static void main(String[] args) {
+        Animal myAnimal = new Dog();
+        myAnimal.makeSound(); // Calls overridden method in Dog class
+    }
+}
