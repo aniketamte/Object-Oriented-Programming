c45dc06631efc1bdb6ece8145ae609198deabb37
aniketamte <aniketamte123@gmail.com>
2025-04-01 11:58:27 +0530

    OOP in java

--- OOPS.java
+++ OOPS.java
@@ -3,6 +3,12 @@ public class OOPS {
             Pen p1 = new Pen();  //Created a pen object called p1
             p1.setColor("Blue");
             System.out.println(p1.color);
+
+            p1.setTip(5);
+            System.out.println(p1.tip);
+
+            p1.setColor("Yellow");
+            System.out.println(p1.color);
       }
 }
 
