dd4be9fa588506ab3166a7665d54eee157efee74
aniketamte <aniketamte123@gmail.com>
2025-04-01 23:02:54 +0530

    OOP in Java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,4 +1,17 @@
+/*
+ * #### Polymorphism in OOP (Java)
+ * 
+ * ====> Polymorphism is one of the core concepts of Object-Oriented Programming (OOP) that allows objects of different classes to be treated as objects of a common superclass. It enables one interface, multiple implementations, making code more flexible and reusable.
+ * 
+ * 
+ * ###Types of Polymorphism in Java
+ * 
+ * 1) Compile-time Polymorphism (Method Overloading)
+ * 
+ * ===> Method overloading allows multiple methods in the same class to have the same name but with different parameters (number, type, or sequence).
 
+ * ===> The compiler determines which method to call based on the method signature at compile time.
+ */
 public class Polymorphism {
 
     public static void main(String[] args) {
