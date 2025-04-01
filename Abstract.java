594f8cf1115f04f8a6d9e5ade1ab1becb8c28d69
aniketamte <aniketamte123@gmail.com>
2025-04-01 23:28:04 +0530

    OOP in Java

--- Abstract.java
+++ Abstract.java
@@ -1,4 +1,14 @@
+/*
+#### Abstract Classes and Methods : ->
+ * Data abstraction is the process of hiding certain details and showing only essential information to the user.
+Abstraction can be achieved with either abstract classes or interfaces (which you will learn more about in the next chapter).
 
+The abstract keyword is a non-access modifier, used for classes and methods:
+
+====> Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class).
+
+====> Abstract method: can only be used in an abstract class, and it does not have a body. The body is provided by the subclass (inherited from).
+ */
 public class Abstract {
 
     public static void main(String[] args) {
